647e9f74217cf2ac80e119a28b60dc356649f1dd
CoderWarrior48 <dawson@hamera.com>
2024-01-30 09:45:11 -0800

    new changes

--- ProgrammingClub/src/app/components/navbar/navbar.component.ts
+++ ProgrammingClub/src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   links = [
-    {name: 'test', path: 'home'}
+    {name: 'Contact', path: 'home'},
+    {name: 'Portfolio', path: 'home'},
   ]
 }
