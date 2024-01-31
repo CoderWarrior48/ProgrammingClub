be34651b8a64d5d12e9806058cb298d50088f35e
CoderWarrior48 <dawson@hamera.com>
2024-01-31 12:02:01 -0800

    added carousel

--- ProgrammingClub/src/app/pages/home/home.component.ts
+++ ProgrammingClub/src/app/pages/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  public slides = [
+    { src: "assets/images/game_design.jpg" },
+    { src: "assets/images/web_development.png" },
+    { src: "assets/images/data_science.jpg" },
+  ];
 }
